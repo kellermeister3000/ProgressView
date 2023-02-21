54c14f2884df99c954d6bbad402a5f9902061e96
Philip Keller <philipjoelkeller@gmail.com>
2023-02-21 13:10:49 -0800

    Initial Commit

---
+++ ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by Philip Keller on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
